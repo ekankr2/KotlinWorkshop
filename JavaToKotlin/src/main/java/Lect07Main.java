c22d83aae7b7f74dcf9e9c37f96c89326fd8b132
ekankr2 <ekankr2@naver.com>
2022-10-11 20:07:21 +0900

    Java file reader class

---
+++ JavaToKotlin/src/main/java/Lect07Main.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Lect07Main {
+    public static void main(String[] args) throws IOException {
+        JavaFilePrinter printer = new JavaFilePrinter();
+        printer.readFile();
+    }
+}
