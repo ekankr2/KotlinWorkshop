f1f4b9d6e591712590e59c99b9d69cbc35df0d0c
ekankr2 <ekankr2@naver.com>
2022-10-12 19:33:02 +0900

    부생성자 대신 default parameter

--- JavaToKotlin/src/main/kotlin/PersonKotlin.kt
+++ JavaToKotlin/src/main/kotlin/PersonKotlin.kt
@@ -1,22 +1,13 @@
 fun main() {
-    PersonKotlin()
 }
 
 class PersonKotlin(
-    val name: String, var age: Int
+    val name: String = "임익환", var age: Int = 1
 ) {
     init {
         if (age <= 0) {
             throw IllegalArgumentException("나이는 ${age} 일 수 없습니다.")
         }
         println("초기화 블록")
-    }
-
-    constructor(name: String) : this(name, 1) {
-        println("첫 번째 부생성자")
-    }
-
-    constructor() : this("홍길동") {
-        println("두 번째 부생성자")
     }
 }
