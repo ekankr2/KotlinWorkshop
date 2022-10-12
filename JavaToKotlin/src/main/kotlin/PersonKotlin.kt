c8a232dc85d4053751fa2492ae2c1bbeabddc90a
ekankr2 <ekankr2@naver.com>
2022-10-12 19:45:33 +0900

    getter expression

--- JavaToKotlin/src/main/kotlin/PersonKotlin.kt
+++ JavaToKotlin/src/main/kotlin/PersonKotlin.kt
@@ -2,12 +2,29 @@ fun main() {
 }
 
 class PersonKotlin(
-    val name: String = "임익환", var age: Int = 1
+    name: String = "임익환", var age: Int = 1
 ) {
+
+    var name = name
+        set(value) {
+            field = value.uppercase()
+        }
+
+    fun getUppercaseName(): String {
+        return this.name.uppercase()
+    }
+
+    val upperCaseName: String
+        get() = this.name.uppercase()
+
     init {
         if (age <= 0) {
             throw IllegalArgumentException("나이는 ${age} 일 수 없습니다.")
         }
         println("초기화 블록")
     }
+
+    val isAdult: Boolean
+        get() = this.age >= 20
+
 }
