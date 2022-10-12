aa776087c3de8302f1eaa0754e13fb312bbfb3c0
ekankr2 <ekankr2@naver.com>
2022-10-12 21:00:38 +0900

    abstract class in Kotlin

---
+++ JavaToKotlin/src/main/kotlin/Lec10/Penguin.kt
@@ -0,0 +1,15 @@
+package Lec10
+
+class Penguin(
+    species: String
+) : Animal(species, 2) {
+
+    private val wingCount: Int = 2
+
+    override fun move() {
+        println("펭귄이 움직인다~")
+    }
+
+    override val legCount: Int
+        get() = super.legCount + this.wingCount
+}
