f3131edc3d5018e99829fb5666e4f5ec83e35764
ekankr2 <ekankr2@naver.com>
2022-10-21 19:25:29 +0900

    구조분해 in normal Class

--- JavaToKotlin/src/main/kotlin/Lec19/Lec19Main.kt
+++ JavaToKotlin/src/main/kotlin/Lec19/Lec19Main.kt
@@ -1,15 +1,21 @@
 package Lec19
 
-data class Person(
+class Person(
     val name: String,
     val age: Int
-)
+) {
+    operator fun component1(): String {
+        return this.name
+    }
+
+    operator fun component2(): Int {
+        return this.age
+    }
+}
 
 fun main() {
     val person = Person("임익환", 100)
-    // val (name, age) = person
-    val name = person.component1()
-    val age = person.component2()
+    val (name, age) = person
 
     println("이름 : ${name}, 나이 : ${age}")
 }
