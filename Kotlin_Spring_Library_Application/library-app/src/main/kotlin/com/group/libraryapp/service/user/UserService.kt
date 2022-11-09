5808f9134755f0c2a5acd3df7b8dffd0ee8acb46
ekankr2 <ekankr2@naver.com>
2022-11-09 20:26:25 +0900

    test: test code for user Loan History

--- Kotlin_Spring_Library_Application/library-app/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ Kotlin_Spring_Library_Application/library-app/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -44,8 +44,8 @@ class UserService(
     }
 
     @Transactional(readOnly = true)
-    fun getUserLoanHistories(): List<UserLoanHistoryResponse>{
-        userRepository.findAll().map { user ->
+    fun getUserLoanHistories(): List<UserLoanHistoryResponse> {
+        return userRepository.findAll().map { user ->
             UserLoanHistoryResponse(
                 name = user.name,
                 books = user.userLoanHistories.map { history ->
